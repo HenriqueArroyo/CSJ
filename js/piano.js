dd2680e2fbf1db661832d3e4cad2d7feb75cba7a
HenriqueArroyo <ana123ca.henrique@gmail.com>
2023-09-22 14:31:53 -0300

    22/09

--- js/piano.js
+++ js/piano.js
@@ -1,11 +1,12 @@
 const keys = document.querySelectorAll('.key');
-        
-        keys.forEach(key => {
-            key.addEventListener('click', () => playSound(key));
-        });
-        
-        function playSound(key) {
-            const note = key.getAttribute('data-note');
-            const audio = new Audio(`sounds/A&.mp3`);
-            audio.play();
-        }
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playSound(key));
+});
+
+function playSound(key) {
+    const note = key.getAttribute('data-note');
+    const audio = new Audio(`sounds/${note}.mp3`); // Use a variável 'note' para carregar o arquivo de som correto
+
+    audio.play();
+}
