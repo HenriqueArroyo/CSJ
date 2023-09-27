3b1badcd2632fd22932bebd74af67a16bb6d59bc
HenriqueArroyo <ana123ca.henrique@gmail.com>
2023-09-27 10:32:40 -0300

    27/09

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,52 +1,53 @@
 // Declarando Variáveis
-var sec = 0;
-var min = 0;
-var hr=0
-var intervalo;
+var sec = 0;  
+var min = 0;  
+var hr = 0;   
+var intervalo;  // usado para pausar o cronômetro
 
-
-// Impede o digito de chegar em 10, retornando-o para 0 e adicionando ao outro digito
-function doisDigitos(digito){
-    if(digito<10){
-        return('0'+digito)
-    }else{
-        return(digito)
+// garante que os números de horas, minutos e segundos sempre sejam exibidos com dois dígitos
+function doisDigitos(digito) {
+    if (digito < 10) {
+        return ('0' + digito);
+    } else {
+        return (digito);
     }
 }
 
-// Função para Iniciar o Cronometro
-function iniciar(){
-    relogio()
-    intervalo= setInterval(relogio,10)
+// Função para Iniciar o Cronômetro
+function iniciar() {
+    relogio();
+    intervalo = setInterval(relogio, 10);  // A cada 10 milissegundos, a função relogio() será chamada para atualizar o cronômetro.
 }
 
-// Função para Pausar o Cronometro
-function pausar(){
-    clearInterval(intervalo)
-
+// Função para Pausar o Cronômetro
+function pausar() {
+    clearInterval(intervalo);  // Para o intervalo, pausando o cronômetro.
 }
 
-
-// Função para Parar o Cronometro // Mostrar mensagem que demonstra o tempo em que o Cronometro foi Parado
-function parar(){
-    clearInterval(intervalo)
-    sec=0
-    min=0
-    window.alert("Tempo parado em  "+document.getElementById('relogio').innerText)
-    document.getElementById('relogio').innerText='00:00:00'
-
+// Função para Parar o Cronômetro e mostrar uma mensagem com o tempo decorrido
+function parar() {
+    clearInterval(intervalo);  // Para o intervalo, interrompendo o cronômetro.
+    // reinicia os atributos
+    sec = 0; 
+    min = 0;  
+    window.alert("Tempo parado em " + document.getElementById('relogio').innerText);  // Exibe uma mensagem com o tempo decorrido
+    document.getElementById('relogio').innerText = '00:00:00';  // Reinicia a exibição do cronômetro para 00:00:00
 }
 
-// Dinâmica de Tempo do Relógio, Quando se chega ao limite da medida se adiciona a próxima medida e se zera o digito
-function relogio(){
-    sec++
-    if(sec==60){
-        min++
-        sec=0
-        if(min==60){
-            min=0
-            hr++
+// Dinâmica de Tempo do Relógio, incrementa os segundos, minutos e horas, ajustando quando eles alcançam seu limite
+function relogio() {
+    sec++;  // Incrementa os segundos
+
+    if (sec == 60) {
+        min++;  // Se os segundos chegarem a 60, incrementa os minutos e redefine os segundos para 0
+        sec = 0;
+
+        if (min == 60) {
+            min = 0;  // Se os minutos chegarem a 60, incrementa as horas e redefine os minutos para 0
+            hr++;
         }
     }
-    document.getElementById('relogio').innerText=doisDigitos(hr)+':'+doisDigitos(min)+':'+doisDigitos(sec)
+
+    // Atualiza o elemento HTML com a exibição formatada do tempo
+    document.getElementById('relogio').innerText = doisDigitos(hr) + ':' + doisDigitos(min) + ':' + doisDigitos(sec);
 }
--- js/piano.js
+++ js/piano.js
@@ -1,11 +1,15 @@
+// Seleciona todos os elementos com a classe 'key'
 const keys = document.querySelectorAll('.key');
 
+// Função para tocar o som a sua respectiva nota.
 function playSound(note) {
+    // Cria um novo elemento de áudio e atribui o arquivo de som com base na nota selecionada.
     const audio = new Audio(`sounds/${note}.mp3`);
-    audio.currentTime = 0;
-    audio.play();
+    audio.currentTime = 0;  // Reinicia o áudio para o início, permitindo tocar várias vezes rapidamente.
+    audio.play();  // Toca o som.
 }
 
+// Mapeamento de teclas do teclado 
 const keyMap = {
     a: 'A',
     s: 'D',
@@ -14,36 +18,41 @@ const keyMap = {
     g: 'E',
     h: 'G',
     j: 'C',
-    
 };
 
 keys.forEach((key) => {
+    // Quando um elemento é clicado, adiciona a classe 'active' e toca o som selecionado.
     key.addEventListener('mousedown', () => {
         key.classList.add('active');
         playSound(key.dataset.note);
     });
 
+    // Quando o botão do mouse é liberado, remove a classe 'active'.
     key.addEventListener('mouseup', () => {
         key.classList.remove('active');
     });
 
+    // Quando o cursor deixa o elemento, remove a classe 'active'.
     key.addEventListener('mouseleave', () => {
         key.classList.remove('active');
     });
 });
 
+// funcionalidade teclas do teclado.
 window.addEventListener('keydown', (e) => {
+    // Verifica se a tecla pressionada corresponde a uma nota no mapeamento.
     if (keyMap[e.key.toLowerCase()]) {
-        playSound(keyMap[e.key.toLowerCase()]);
+        playSound(keyMap[e.key.toLowerCase()]);  // Toca o som da tecla pressionada.
         const keyElement = document.querySelector(`[data-note="${keyMap[e.key.toLowerCase()]}"]`);
         if (keyElement) {
-            keyElement.classList.add('active');
+            keyElement.classList.add('active');  // Adiciona a classe 'active' ao elemento selecionado.
         }
     }
 });
 
+// Funcionalidade quando as teclas do teclado são liberadas.
 window.addEventListener('keyup', (e) => {
-    // Remova a classe 'active' quando a tecla for liberada
+    // Remove a classe 'active' quando a tecla é liberada.
     const keyElement = document.querySelector(`[data-note="${keyMap[e.key.toLowerCase()]}"]`);
     if (keyElement) {
         keyElement.classList.remove('active');
